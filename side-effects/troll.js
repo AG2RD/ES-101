a955483a1ebbbbdee0d1aa7b7c0deae7146bafe7
Adrien Gerard <agerard@altissia.com>
2017-05-06 08:57:16 +0200

    structure files modification

---
+++ side-effects/troll.js
@@ -0,0 +1,9 @@
+// addition
+console.log(1 + 1); //2
+console.log("1" + 1); //11
+console.log(1 + "1"); //11
+console.log("1" + "1"); //11
+
+// substraction
+console.log("2" - "1"); //1
+console.log("2" - "-2"); //4
